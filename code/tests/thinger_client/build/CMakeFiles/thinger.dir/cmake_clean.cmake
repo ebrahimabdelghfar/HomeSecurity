39ee2c557a3d9b5cfd5bc561859c05cdeddda33c
Apolo151 <abdogofor55@gmail.com>
2023-05-29 16:45:02 +0100

    Changes to be committed: 	modified:   code/tests/thinger_client/CMakeLists.txt 		the change: delete IF(raspberry) that cause the problem of the building 	new file:   code/tests/thinger_client/WiringPi

---
+++ code/tests/thinger_client/build/CMakeFiles/thinger.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/thinger.dir/src/main.cpp.o"
+  "thinger"
+  "thinger.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/thinger.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
